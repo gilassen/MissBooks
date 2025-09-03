463d056e6f23135edf41d47c40acbb2e644840ec
gilassen <gil.assenhaym@gmail.com>
2025-09-03 14:45:01 +0300

    fix(book-preview): show authors correctly on card

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -1,15 +1,15 @@
 export function BookPreview({ book }) {
+  const authorsText =
+    (Array.isArray(book.authors) && book.authors.length)
+      ? book.authors.join(', ')
+      : (book.author || 'Unknown')  
+
   return (
     <article className="book-preview">
       <h2 className="title">{book.title}</h2>
-      <h4>By: {book.author}</h4>
-
+      <h4>By: {authorsText}</h4>
       {book.thumbnail && (
-        <img
-          className="book-thumbnail"
-          src={book.thumbnail}
-          alt={book.title}
-        />
+        <img className="book-thumbnail" src={book.thumbnail} alt={book.title} />
       )}
     </article>
   )
